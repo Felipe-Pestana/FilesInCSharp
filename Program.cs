bbffaf0b482d1db6fbc5f54cedb1dbcfb7f61bb3
Felipe Pestana <pestana@sitic.com.br>
2023-03-21 00:06:15 -0300

    Arquivo Completo

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Text;
+string filename = "texto.txt";
+string path = @"C:\\Users\\" + System.Environment.UserName + "\\";
+//Console.Write(path);
+
+using (var file = new StreamWriter(path + filename))
+{
+    var text = new StringBuilder();
+    file.Write(text);
+
+    Console.WriteLine($"\nArquivo salvo em " + path + filename);
+    Console.ReadKey();
+}
+
+using (var file = new StreamReader(path + filename))
+{
+    Console.Clear();
+    
+    var text = file.ReadToEnd();
+
+    Console.WriteLine(text);
+    Console.ReadKey();
+}
